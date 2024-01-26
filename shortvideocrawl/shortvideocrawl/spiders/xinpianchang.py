77819c2c25114359dbae964c86238dea69ec9d0f
zeohan.dxs <zeohan.dxs@gmail.com>
2024-01-26 23:37:45 +0800

    basic version

--- shortvideocrawl/shortvideocrawl/spiders/xinpianchang.py
+++ shortvideocrawl/shortvideocrawl/spiders/xinpianchang.py
@@ -3,16 +3,21 @@ from urllib.parse import urlencode
 
 import scrapy
 
+from ..items import ShortvideocrawlItem
+
 GET_SEARCH_ID_API = "https://www.xinpianchang.com/api/xpc/v2/search/getSearchKwIdByKw"
 SEARCH_API = "https://www.xinpianchang.com/_next/data/%s/search.json"
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
 }
+VIDEO_INFO_API = (
+    "https://mod-api.xinpianchang.com/mod/api/v2/media/%s?appKey=61a2f329348b3bf77"
+)
 
 
 class XinpianchangSpider(scrapy.Spider):
     name = "xinpianchang"
-    allowed_domains = ["www.xinpianchang.com"]
+    allowed_domains = ["www.xinpianchang.com", "mod-api.xinpianchang.com"]
 
     query = "蔡徐坤"
     count = 20
@@ -55,23 +60,28 @@ class XinpianchangSpider(scrapy.Spider):
         search_data = resp["pageProps"]["searchData"]
         if "list" in search_data.keys():
             for d in search_data["list"]:
-                print(d["web_url"])
-                # yield scrapy.Request(
-                #     d["web_url"].split("?")[0],
-                #     headers=headers,
-                #     callback=self.parse_detail,
-                # )
-                # break
+                # print(d["web_url"])
+                yield scrapy.Request(
+                    d["web_url"].split("?")[0],
+                    headers=headers,
+                    callback=self.parse_detail,
+                )
 
-    # def parse_detail(self, response):
-    #     print(response.body)
-    # resp = json.loads(response.body)
-    # search_data = resp["pageProps"]["searchData"]
-    # if "list" in search_data.keys():
-    #     for d in search_data["list"]:
-    #         # print(d["web_url"])
-    #         yield scrapy.Request(
-    #             d["web_url"].split("?")[0],
-    #             headers=headers,
-    #             callback=self.parse_detail,
-    #         )
+    def parse_detail(self, response):
+        # print(response.body)
+        data = response.xpath('//*[@id="__NEXT_DATA__"]/text()').get()
+        vid = json.loads(data)["props"]["pageProps"]["detail"]["vid"]
+        yield scrapy.Request(
+            VIDEO_INFO_API % vid,
+            headers=headers,
+            callback=self.parse_video_info,
+        )
+
+    def parse_video_info(self, response):
+        resp = json.loads(response.body)
+        video_url = resp["data"]["resource"]["progressive"][0]["url"]
+        video_id = resp["data"]["mid"]
+        yield ShortvideocrawlItem(
+            id=video_id,
+            file_urls=[video_url],
+        )
