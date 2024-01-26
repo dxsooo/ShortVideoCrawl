67ad549abd7d258110389aede83bcda4b4947a1a
zeohan.dxs <zeohan.dxs@gmail.com>
2024-01-26 23:01:33 +0800

    get buildId to concat search url

--- shortvideocrawl/shortvideocrawl/spiders/xinpianchang.py
+++ shortvideocrawl/shortvideocrawl/spiders/xinpianchang.py
@@ -4,7 +4,7 @@ from urllib.parse import urlencode
 import scrapy
 
 GET_SEARCH_ID_API = "https://www.xinpianchang.com/api/xpc/v2/search/getSearchKwIdByKw"
-SEARCH_API = "https://www.xinpianchang.com/_next/data/Y8h4CTmf1sCzFZr0v4V_p/search.json"
+SEARCH_API = "https://www.xinpianchang.com/_next/data/%s/search.json"
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
 }
@@ -24,13 +24,13 @@ class XinpianchangSpider(scrapy.Spider):
             headers=headers,
         )
 
-    def search_request(self, search_id):
+    def search_request(self, search_id, build_id):
         params = {
             "from": "inputSearch",
             "kw_id": search_id,
         }
         return scrapy.Request(
-            SEARCH_API + "?" + urlencode(params),
+            SEARCH_API % build_id + "?" + urlencode(params),
             headers=headers,
             callback=self.parse_search,
         )
@@ -38,7 +38,17 @@ class XinpianchangSpider(scrapy.Spider):
     def parse_search_id(self, response):
         resp = json.loads(response.body)
         search_id = resp["data"]["id"]
-        yield self.search_request(search_id)
+        yield scrapy.Request(
+            "https://www.xinpianchang.com/",
+            headers=headers,
+            callback=self.parse_build_info,
+            meta={"search_id": search_id},
+        )
+
+    def parse_build_info(self, response):
+        data = response.xpath('//*[@id="__NEXT_DATA__"]/text()').get()
+        build_id = json.loads(data)["buildId"]
+        yield self.search_request(response.meta["search_id"], build_id)
 
     def parse_search(self, response):
         resp = json.loads(response.body)
@@ -51,3 +61,17 @@ class XinpianchangSpider(scrapy.Spider):
                 #     headers=headers,
                 #     callback=self.parse_detail,
                 # )
+                # break
+
+    # def parse_detail(self, response):
+    #     print(response.body)
+    # resp = json.loads(response.body)
+    # search_data = resp["pageProps"]["searchData"]
+    # if "list" in search_data.keys():
+    #     for d in search_data["list"]:
+    #         # print(d["web_url"])
+    #         yield scrapy.Request(
+    #             d["web_url"].split("?")[0],
+    #             headers=headers,
+    #             callback=self.parse_detail,
+    #         )
