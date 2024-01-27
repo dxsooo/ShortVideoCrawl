d8ea5e5257a75b6f09251efed9f3edff961a91e3
zeohan.dxs <zeohan.dxs@gmail.com>
2024-01-28 01:13:17 +0800

    update kuaishou

--- shortvideocrawl/shortvideocrawl/spiders/kuaishou.py
+++ shortvideocrawl/shortvideocrawl/spiders/kuaishou.py
@@ -13,13 +13,20 @@ cookies = {
     "kpf": "PC_WEB",
     "kpn": "KUAISHOU_VISION",
     "clientid": 3,
-    # "client_key": "65890b29",
-    # "didv": "1673605994679",
+    "client_key": "65890b29",
+    "didv": "1673605994679",
     "did": "web_35b63ae980e92610232940034cb2dc66",
 }
 
-# kpf=PC_WEB; clientid=3; did=web_e09a1040aa48f4986189eb05785f0d70; kpn=KUAISHOU_VISION
+# kpf = PC_WEB
+# clientid = 3
+# didv = 1673605994679
+# did = web_35b63ae980e92610232940034cb2dc66
+# clientid = 3
+# kpn = KUAISHOU_VISION
 
+# kpf=PC_WEB; clientid=3; did=web_e09a1040aa48f4986189eb05785f0d70; kpn=KUAISHOU_VISION
+# kpf=PC_WEB; clientid=3; client_key=65890b29; didv=1673605994679; did=web_35b63ae980e92610232940034cb2dc66; clientid=3; kpn=KUAISHOU_VISION
 SEARCH_API = "https://www.kuaishou.com/graphql"
 
 
@@ -40,9 +47,8 @@ class KuaishouSpider(scrapy.Spider):
                 "keyword": self.query,
                 "pcursor": str(page),
                 "page": "search",
-                "searchSessionId": "MTRfMF8xNjc1MDY3MDgzNzM2X-iUoeW-kOWdpF8yMDI2",
             },
-            "query": "fragment photoContent on PhotoEntity {\n  id\n  duration\n  caption\n  originCaption\n  likeCount\n  viewCount\n  realLikeCount\n  coverUrl\n  photoUrl\n  photoH265Url\n  manifest\n  manifestH265\n  videoResource\n  coverUrls {\n    url\n    __typename\n  }\n  timestamp\n  expTag\n  animatedCoverUrl\n  distance\n  videoRatio\n  liked\n  stereoType\n  profileUserTopPhoto\n  musicBlocked\n  __typename\n}\n\nfragment feedContent on Feed {\n  type\n  author {\n    id\n    name\n    headerUrl\n    following\n    headerUrls {\n      url\n      __typename\n    }\n    __typename\n  }\n  photo {\n    ...photoContent\n    __typename\n  }\n  canAddComment\n  llsid\n  status\n  currentPcursor\n  tags {\n    type\n    name\n    __typename\n  }\n  __typename\n}\n\nquery visionSearchPhoto($keyword: String, $pcursor: String, $searchSessionId: String, $page: String, $webPageArea: String) {\n  visionSearchPhoto(keyword: $keyword, pcursor: $pcursor, searchSessionId: $searchSessionId, page: $page, webPageArea: $webPageArea) {\n    result\n    llsid\n    webPageArea\n    feeds {\n      ...feedContent\n      __typename\n    }\n    searchSessionId\n    pcursor\n    aladdinBanner {\n      imgUrl\n      link\n      __typename\n    }\n    __typename\n  }\n}\n",
+            "query": "fragment photoContent on PhotoEntity {\n  __typename\n  id\n  duration\n  caption\n  originCaption\n  likeCount\n  viewCount\n  commentCount\n  realLikeCount\n  coverUrl\n  photoUrl\n  photoH265Url\n  manifest\n  manifestH265\n  videoResource\n  coverUrls {\n    url\n    __typename\n  }\n  timestamp\n  expTag\n  animatedCoverUrl\n  distance\n  videoRatio\n  liked\n  stereoType\n  profileUserTopPhoto\n  musicBlocked\n}\n\nfragment recoPhotoFragment on recoPhotoEntity {\n  __typename\n  id\n  duration\n  caption\n  originCaption\n  likeCount\n  viewCount\n  commentCount\n  realLikeCount\n  coverUrl\n  photoUrl\n  photoH265Url\n  manifest\n  manifestH265\n  videoResource\n  coverUrls {\n    url\n    __typename\n  }\n  timestamp\n  expTag\n  animatedCoverUrl\n  distance\n  videoRatio\n  liked\n  stereoType\n  profileUserTopPhoto\n  musicBlocked\n}\n\nfragment feedContent on Feed {\n  type\n  author {\n    id\n    name\n    headerUrl\n    following\n    headerUrls {\n      url\n      __typename\n    }\n    __typename\n  }\n  photo {\n    ...photoContent\n    ...recoPhotoFragment\n    __typename\n  }\n  canAddComment\n  llsid\n  status\n  currentPcursor\n  tags {\n    type\n    name\n    __typename\n  }\n  __typename\n}\n\nquery visionSearchPhoto($keyword: String, $pcursor: String, $searchSessionId: String, $page: String, $webPageArea: String) {\n  visionSearchPhoto(keyword: $keyword, pcursor: $pcursor, searchSessionId: $searchSessionId, page: $page, webPageArea: $webPageArea) {\n    result\n    llsid\n    webPageArea\n    feeds {\n      ...feedContent\n      __typename\n    }\n    searchSessionId\n    pcursor\n    aladdinBanner {\n      imgUrl\n      link\n      __typename\n    }\n    __typename\n  }\n}\n",
         }
         return scrapy.Request(
             SEARCH_API,
